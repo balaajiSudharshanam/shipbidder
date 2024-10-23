55e67282e2b6dd140d7a59675d6ce39c467b1316
balaaji <balaajiuma@gmail.com>
2024-10-24 11:21:08 +1300

    Created auction , location and item controller

--- backend/Controllers/AutionControler.js
+++ backend/Controllers/AutionControler.js
@@ -1,39 +1,49 @@
-const Auction=require('../Model/AuctionModel');
+const Auction = require('../Model/AuctionModel');
 const asyncHandler = require('express-async-handler');
 
+const createAuction = asyncHandler(async (req, res) => {
+  const {
+    jobTitle,
+    jobDescription,
+    jobProvider,
+    pickupLocation,
+    dropLocation,
+    item,
+    pickupDateTime,
+    dropDateTime
+  } = req.body;
 
-const createAuction= asyncHandler(async(req,res)=>{
-    const {
-        jobTitle,
-        jobDescription,
-        jobProvider,
-        pickupLocation,
-        dropLocation,
-        item,
-        pickupDateTime,
-        dropDateTime
-      }=req.body;
+  // Validate required fields
+  if (!jobTitle || !jobProvider || !pickupLocation || !dropLocation || !item || !pickupDateTime || !dropDateTime) {
+    return res.status(400).json({ message: 'Please provide all required fields' });
+  }
+
+  // Create auction by passing object to Auction.create
+  const auction = await Auction.create({
+    jobTitle,
+    jobDescription,
+    jobProvider,
+    pickupLocation,
+    dropLocation,
+    item,
+    pickupDateTime,
+    dropDateTime
+  });
+
+  if (auction) {
+    
+    const populatedAuction = await auction
+      .populate('jobProvider', 'name email') // Populate user details (job provider)
+      .populate('pickupLocation', 'longitude latitude') // Populate pickup location
+      .populate('dropLocation', 'longitude latitude') // Populate drop location
+      .populate('item') // Populate item details
       
-      if (!jobTitle || !jobProvider || !pickupLocation || !dropLocation || !item || !pickupDateTime || !dropDateTime) {
-        return res.status(400).json({ message: 'Please provide all required fields' });
-      }
 
-      const auction= await Auction.create(jobTitle,
-        jobDescription,
-        jobProvider,
-        pickupLocation,
-        dropLocation,
-        item,
-        pickupDateTime,
-        dropDateTime);
-        if(auction){
-          res.status(202).json({
-            id: auction._id,
-
-          })
-        }else{
-          res.status(400);
-           throw new Error("Failed to create new user");
-        }
+    return res.status(201).json(populatedAuction); // Return populated auction
+  } else {
+    res.status(400);
+    throw new Error('Failed to create new auction');
+  }
 });
-module.exports={createAuction};
+
+module.exports = { createAuction };
